d72033c3eb864cd06868017940aca588c696fe6f
Aaron Patterson <aaron.patterson@gmail.com>
2012-04-25 14:12:08 -0500

    use a pipe to put the main process to sleep

--- lib/resque/worker.rb
+++ lib/resque/worker.rb
@@ -128,7 +128,9 @@ module Resque
       loop do
         break if shutdown?
 
-        if not paused? and job = reserve
+        pause if should_pause?
+
+        if job = reserve
           log "got: #{job.inspect}"
           job.worker = self
           run_hook :before_fork, job
@@ -268,7 +270,6 @@ module Resque
         trap('QUIT') { shutdown   }
         trap('USR1') { kill_child }
         trap('USR2') { pause_processing }
-        trap('CONT') { unpause_processing }
       rescue ArgumentError
         warn "Signals QUIT, USR1, USR2, and/or CONT not supported."
       end
@@ -309,8 +310,21 @@ module Resque
     end
 
     # are we paused?
-    def paused?
+    def should_pause?
       @paused
+    end
+    alias :paused? :should_pause?
+
+    def pause
+      rd, wr = IO.pipe
+      trap('CONT') {
+        log "CONT received; resuming job processing"
+        @paused = false
+        wr.write 'x'
+        wr.close
+      }
+      rd.read 1
+      rd.close
     end
 
     # Stop processing jobs after the current one has completed (if we're
@@ -318,12 +332,6 @@ module Resque
     def pause_processing
       log "USR2 received; pausing job processing"
       @paused = true
-    end
-
-    # Start processing jobs again after a pause
-    def unpause_processing
-      log "CONT received; resuming job processing"
-      @paused = false
     end
 
     # Looks for any workers which should be running on this server
